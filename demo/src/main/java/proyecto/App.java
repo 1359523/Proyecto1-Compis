2db852bf31fb6de6ccff29f077a097931d1d54aa
1359523 <armoratay@correo.url.edu.gt>
2025-05-03 15:00:54 -0600

    Se cambio el proyecto App.Java para que el input sea leido desde un archivo de texto

--- demo/src/main/java/proyecto/App.java
+++ demo/src/main/java/proyecto/App.java
@@ -1,8 +1,12 @@
 package proyecto;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+
+//Imports de antlr
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
-
 import proyecto.antlr.ExprLexer;
 import proyecto.antlr.ExprParser;
 
@@ -10,10 +14,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        String input = "print (" + '"' + "Hello World" + '"' + ");";
+        String ruta = "C:\\antlr\\Proyecto_Fase2\\Proyecto_Fase2\\prueba2.txt";
+        String contenido = "";
+        try{
+            contenido = Files.readString(Path.of(ruta));            
+        } catch (IOException e){
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+
+        System.out.println(contenido);
 
         //Crear lexer y parser
-        ExprLexer lexer = new ExprLexer(CharStreams.fromString(input));
+        ExprLexer lexer = new ExprLexer(CharStreams.fromString(contenido));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         ExprParser parser = new ExprParser(tokens);
 
@@ -23,8 +35,8 @@ public class App
         //Usar el visitor
         Evaluador visitor = new Evaluador();
         
-        int resultado = visitor.visit(tree);
+        visitor.visit(tree);
 
-        System.out.println("Resultado: " + resultado);
+        //System.out.println("Resultado: " + resultado);
     }
 }
